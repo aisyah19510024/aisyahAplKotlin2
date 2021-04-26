538b26d73e37ae22715b9e30827213063a6b057b
aisyah19510024 <81897714+aisyah19510024@users.noreply.github.com>
2021-04-26 11:40:48 +0800

    RecyclerView

--- app/src/main/java/com/example/datateman/MyFriendsFragment.kt
+++ app/src/main/java/com/example/datateman/MyFriendsFragment.kt
@@ -6,21 +6,71 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_my_friends.*
 
-class FriendsFragment : Fragment() {
+class MyFriendsFragment : Fragment(){
+
+    lateinit var listTeman:ArrayList<MyFriend>
+
+    private fun simulasiDataTeman(){
+        listTeman= ArrayList()
+        listTeman.add(
+                MyFriend("Aisyah Indah", "Perempuan", "aisyah@gmail.com", "085985697319",
+                        "Malang")
+        )
+        listTeman.add(
+                MyFriend("mulan", "Perempuan", "mulan@gmail.com", "081232356456",
+                        "Balikpapan")
+        )
+        listTeman.add(
+                MyFriend("Dewi", "Perempuan", "dewi@gmail.com", "081363625451",
+                        "Sidoarjo")
+        )
+        listTeman.add(
+                MyFriend("Dewa", "Laki-laki", "dewa@gmail.com", "085695789456",
+                        "Makasar")
+        )
+        listTeman.add(
+                MyFriend("Yudha", "Laki-laki", "Yudha@gmail.com", "082456789123",
+                        "Maluku")
+        )
+        listTeman.add(
+                MyFriend("Duwi", "Laki-laki", "duwi@gmail.com", "081478512356",
+                        "jakarta")
+        )
+    }
+
+    private fun tampilTeman(){
+        rv_listMyfriends.layoutManager=LinearLayoutManager(activity)
+        rv_listMyfriends.adapter=MyFriendAdapter(activity!!, listTeman)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_my_friends, container, false)
     }
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    private fun initView(){
+        simulasiDataTeman()
+        tampilTeman()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
     }
 }
